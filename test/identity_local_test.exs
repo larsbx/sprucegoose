8a7a61785b9af4e2e14852afc38c089445cb63b5
Lars Bendixen <larsbendixen@gmail.com>
2026-07-30 00:25:12 +0000

    Prove identity sequence restart durability

--- test/identity_local_test.exs
+++ test/identity_local_test.exs
@@ -36,6 +36,26 @@ defmodule SpruceGoose.IdentityLocalTest do
     assert %{rows: [[1]]} = Repo.query!("SELECT count(*) FROM spruce_goose_identity", [])
   end
 
+  test "origin sequence remains monotone across a database client restart" do
+    opts = Repo.config() |> Keyword.take([:hostname, :port, :username, :password, :database])
+
+    {:ok, first_client} = Postgrex.start_link(opts)
+
+    %{rows: [[first]]} =
+      Postgrex.query!(first_client, "SELECT nextval('spruce_goose_origin_seq')", [])
+
+    GenServer.stop(first_client)
+
+    {:ok, second_client} = Postgrex.start_link(opts)
+
+    %{rows: [[second]]} =
+      Postgrex.query!(second_client, "SELECT nextval('spruce_goose_origin_seq')", [])
+
+    GenServer.stop(second_client)
+
+    assert second > first
+  end
+
   test "database rejects mutation of either key half" do
     Local.peer_id()
     {other_public, other_private} = :crypto.generate_key(:eddsa, :ed25519)
